d6386d7a2fee7569ad0199f9a82776503ef1ca65
tarkanbatar <tarkanbatar4@gmail.com>
2022-01-11 09:40:32 +0300

    feat: checkink process by document completed

---
+++ cli/check.php
@@ -0,0 +1,28 @@
+<?php
+
+require '../vendor/autoload.php';
+
+// Get object
+$eDevlet = new \EDevlet\Dogrula();
+// $eDevlet->verbose = true;
+
+// Output version
+echo sprintf('E-Devlet Belge Doğrulama %s' . "\r\n", $eDevlet->version);
+echo 'Usage: php check.php [filename]' . "\r\n";
+
+// Get arguments
+$args = array_values(array_diff_key($argv, array(basename(__FILE__))));
+
+// Check arguments
+if (count($args) < 1) die('Wrong arguments passed!' . "\r\n");
+
+// Get file
+$file = $args[0];
+if (!is_file($file) || !file_exists($file)) die('file not found!' . "\r\n");
+echo sprintf('Belge: %s' . "\r\n", $file);
+
+// Validate
+echo sprintf('Doğrulanıyor... ');
+$result = $eDevlet->dogrula($file);
+var_dump($result);
+echo sprintf('%s' . "\r\n", $result === true ? 'Geçerli' : ($result === false ? 'Geçersiz' : 'Bağlantı problemi lütfen tekrar deneyin'));
---
+++ src/Dogrula.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace EDevlet;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ConnectException;
+use Smalot\PdfParser\Parser;
+
+/**
+ * Class Dogrula
+ * @package EDevlet
+ */
+class Dogrula
+{
+
+	/**
+	 * Validation steps
+	 * @var array
+	 */
+	protected $steps = array(
+		'parse_pdf',
+		'get_token',
+		'accept_form',
+	);
+
+	/**
+	 * Endpoint
+	 * @var string
+	 */
+	protected $endpoint = 'https://www.turkiye.gov.tr';
+
+	/**
+	 * Version
+	 * @var string
+	 */
+	public $version = '1.0.0';
+
+	/**
+	 * File to be validated
+	 * @var string
+	 */
+	private $file;
+
+	/**
+	 * Kimlik no
+	 * @var string
+	 */
+	private $kimlikNo;
+
+	/**
+	 * Pdf Body
+	 * @var string
+	 */
+	private $pdfBody;
+
+	/**
+	 * Barkod
+	 * @var string
+	 */
+	private $barkod;
+
+	/**
+	 * @var Client
+	 */
+	private $client;
+
+	/**
+	 * @var string
+	 */
+	private $responseBody;
+
+	/**
+	 * Token
+	 * @var string
+	 */
+	private $token;
+
+	/**
+	 * Verbose
+	 * @var bool
+	 */
+	public $verbose = false;
+
+
+	/**
+	 * Doğrulama
+	 * @param string $kimlikNo
+	 * @param string $file
+	 *
+	 * @return bool|mixed
+	 */
+	public function dogrula($file)
+	{
+		$this->file = $file;
+		$result = false;
+
+		// Run steps
+		foreach ($this->steps as $step) {
+			try {
+				$result = call_user_func(array($this, $step));
+				if ($result !== true) {
+					break;
+				}
+			} catch (ConnectException $e) {
+				$result = null;
+				break;
+			} catch (\Exception $e) {
+				$result = false;
+				break;
+			}
+		}
+		// Return result
+		return $result;
+	}
+
+	function getPart($regex, $string)
+	{
+		$m = array();
+		preg_match($regex, $string, $m);
+		return $m && isset($m[1]) ? $m[1] : null;
+	}
+
+	private function parse_pdf()
+	{
+		$this->pdfBody = str_replace("\r\n", "\n", (string)(new Parser())->parseFile($this->file)->getText());
+		$pdf = explode("\n", $this->pdfBody);
+		$this->kimlikNo = $this->getPart('/(\d+)\sT.C. Kimlik No/', $this->pdfBody);
+		$this->barkod = reset($pdf);
+		if (strpos($this->pdfBody, $this->kimlikNo) === false) {
+			return false;
+		}
+		if (empty($this->barkod) || !is_string($this->barkod)) {
+			return false;
+		}
+		return true;
+	}
+
+	private function get_token()
+	{
+		// Create client
+		$this->client = new Client(array(
+			'base_uri' => $this->endpoint,
+			'timeout' => 5,
+			'cookies' => true,
+			'headers' => array(
+				'User-Agent' => 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+			)
+		));
+		// Get token
+		$resp = (string) $this->client->get('/belge-dogrulama')->getBody();
+		$this->token = trim($this->getPart('~data-token="(.+?)"~', $resp));
+
+		if (!$this->token || empty($this->token)) {
+			return false;
+		}
+		return true;
+	}
+
+	private function accept_form()
+	{
+		$resp = (string) $this->client->post('/belge-dogrulama?submit', array(
+			'form_params' => array(
+				'sorgulananTCKimlikNo' => $this->kimlikNo,
+				'sorgulananBarkod' => $this->barkod,
+				'token' => $this->token
+			)
+		))->getBody();
+
+		$this->token = trim($this->getPart('~data-token="(.+?)"~', $resp));
+		$this->responseBody = (string) $this->client->post('/belge-dogrulama?asama=kontrol&submit', array(
+			'form_params' => array(
+				'chkOnay' => 1,
+				'token' => $this->token
+			)
+		))->getBody();
+
+		return true;
+	}
+}
